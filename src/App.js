c4b20f6a8798d072ffa49f4a4a95385f572cf96d
cesararc <cesareyesc@outlook.com>
2023-11-06 08:33:20 +0000

    Se Estructura Bien Las Variables de los archivos

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { LinksToVisit } from './LinksToVisit';
 import { LinkSearch } from './LinkSearch';
 import { LinkList } from './LinkList';
 import { CreateLink } from './CreateLink';
+import { LinkItem } from './LinkItem';
 import './App.css';
 
 function App() {
@@ -19,34 +20,5 @@ function App() {
   );
 }
 
-function LinkItem() {
-  return(
-    <div>
-    <h1>This is the tittle Link</h1>
-    <a
-      className="App-link"
-      href="https://platzi.com/reactjs"
-      target="_blank"
-      rel="noopener noreferrer" 
-    >
-      Aprende React
-    </a>
-    <h3>Things to learn</h3>
-    <ul>
-      <li>
-        <span>V</span>
-        <p> useEffect</p>
-      </li>
-      <li>
-        <span>V</span>
-        <p> useState</p>
-      </li>
-      <li>
-        <span>V</span>
-        <p> useContext</p>
-      </li>
-    </ul>
-  </div>
-  )
-}
+
 export default App;
