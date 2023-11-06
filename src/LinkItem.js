b49ebf17a8f96892d399cf0155c5c17fa5dbc4db
cesararc <cesareyesc@outlook.com>
2023-11-06 22:54:18 +0000

    update

--- src/LinkItem.js
+++ src/LinkItem.js
@@ -1,29 +1,16 @@
 function LinkItem() {
     return(
       <div>
-      <h1>This is the tittle Link</h1>
-      <a
-        className="App-link"
-        href="https://platzi.com/reactjs"
-        target="_blank"
-        rel="noopener noreferrer" 
-      >
-        Aprende React
-      </a>
-      <h3>Things to learn</h3>
+      <h3>This is the tittle Link</h3>
+      <b>Things to learn</b>
       <ul>
         <li>
           <span>V</span>
           <p> useEffect</p>
+          <span>X</span><br/>
+          <a href="https://reactjs.org/docs/hooks-effect.html" >Go</a>
         </li>
-        <li>
-          <span>V</span>
-          <p> useState</p>
-        </li>
-        <li>
-          <span>V</span>
-          <p> useContext</p>
-        </li>
+       
       </ul>
     </div>
     )
--- src/LinkList.js
+++ src/LinkList.js
@@ -1,11 +1,9 @@
-function LinkList() {
+function LinkList(props) {
     return(
         <div>
             <h1>Links to visit and take notes</h1>
             <ul>
-                <li> <a href="https://www.google.com">Google</a> </li>
-                <li> <a href="https://www.facebook.com">Facebook</a> </li>
-                <li> <a href="https://www.youtube.com">Youtube</a> </li>
+                {props.children}
             </ul>
         </div>
                     
