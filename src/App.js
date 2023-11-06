2c5be5e221773427d0295168071cc828c177bba2
cesararc <cesareyesc@outlook.com>
2023-11-06 07:22:08 +0000

    Se Agregra estructura

--- src/App.js
+++ src/App.js
@@ -4,22 +4,46 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Venga Empiza react <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprende React
-        </a>
-      </header>
+      <LinkCounter />
+      <LinkSearch />
+
+      <LinkList>
+        <LinkItem />
+        
+      </LinkList>
+      <CreateLink />
     </div>
   );
 }
 
+function LinkItem() {
+  return(
+    <div>
+    <h1>This is the tittle Link</h1>
+    <a
+      className="App-link"
+      href="https://platzi.com/reactjs"
+      target="_blank"
+      rel="noopener noreferrer" 
+    >
+      Aprende React
+    </a>
+    <h3>Things to learn</h3>
+    <ul>
+      <li>
+        <span>V</span>
+        <p> useEffect</p>
+      </li>
+      <li>
+        <span>V</span>
+        <p> useState</p>
+      </li>
+      <li>
+        <span>V</span>
+        <p> useContext</p>
+      </li>
+    </ul>
+  </div>
+  )
+}
 export default App;
